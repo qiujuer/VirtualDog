340098b508585f8031e81652ac982c8fdf15dccc
Qiujuer <qiujuer@live.cn>
2021-03-02 22:50:25 +0800

    格式化代码

--- app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.os.Bundle
@@ -5,7 +20,18 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -24,7 +50,12 @@ import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.bean.DogModel
 import com.example.androiddevchallenge.bean.ModelFactory
 import com.example.androiddevchallenge.bean.getBaseInfo
-import com.example.androiddevchallenge.ui.theme.*
+import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.TextPrice
+import com.example.androiddevchallenge.ui.theme.TextPrimary
+import com.example.androiddevchallenge.ui.theme.TextPrimaryLight
+import com.example.androiddevchallenge.ui.theme.TextSecondary
+import com.example.androiddevchallenge.ui.theme.typography
 
 class DetailActivity : AppCompatActivity() {
     companion object {
@@ -57,7 +88,6 @@ fun MyAppForDetail(model: DogModel) {
         DetailRoot(model)
     }
 }
-
 
 @Composable
 fun DetailRoot(model: DogModel) {
@@ -142,7 +172,6 @@ fun DetailNavigationView() {
         )
     }
 }
-
 
 @Composable
 fun DetailBaseInfoView(model: DogModel) {
